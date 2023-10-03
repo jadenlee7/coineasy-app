f5838efcf6570b03a4a6a67071b550ae24899c93
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-03 10:52:28 +0200

    fix bug profile not displayed at full height

--- components/ProfileDetails.js
+++ components/ProfileDetails.js
@@ -103,7 +103,7 @@ export default function ProfileDetails({profile, pfpMarginTop = -10}) {
   }
 
   return(
-    <View>
+    <View style={{flex: 1}}>
       <Animated.ScrollView onScroll={scrollHandler} scrollEventThrottle={16} style={{backgroundColor: 'white'}}>
         {/** Display profile details */}
         <View style={[tailwind('flex flex-col items-center')]}>
