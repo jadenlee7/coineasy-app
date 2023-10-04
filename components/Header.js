8244cd6eacbeb49e1f37ee71b960e36daa84aea6
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-04 13:05:00 +0200

    Fix shaky screen on scroll

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { Dimensions, SafeAreaView, StyleSheet, Text, View, Image, NativeEventEmitter, NativeModules, Platform, StatusBar } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, View, Image, NativeEventEmitter, NativeModules, Platform, StatusBar, Animated } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 import { GlobalContext } from "../contexts/GlobalContext";
 import useStatusBarHeight from "../hooks/useStatusBarHeight";
-import Animated, {
+import {
   withTiming,
   useAnimatedStyle,
   useSharedValue,
   Easing
 } from 'react-native-reanimated';
+import SecondHeader from "./SecondHeader";
 
 export default function Header() {
-  const { screen, scrolled, setScrolled, postDetailsVis, profileSelected, translateY } = useContext(GlobalContext);
+  const { screen, scrolled, setScrolled, postDetailsVis, profileSelected, translateY, setCategory, setScreen, previousScreen, category, scrollAnim,offsetAnim, setClampedScroll, navbarTranslate } = useContext(GlobalContext);
   const tailwind = useTailwind();
   const { width } = Dimensions.get('window');
   const statusBarHeight = useStatusBarHeight();
@@ -27,16 +28,50 @@ export default function Header() {
     };
   });
 
-
   if(screen == "qr") {
     return;
   }
+  
+  function resetCategory() {
+    setCategory(null);
+    setScreen(previousScreen);
+  }
 
   return(
-    <Animated.View style={[tailwind('w-full'), { height: 40 + statusBarHeight }, actionBarStyle]}>
+    <Animated.View 
+        style={[
+            tailwind('w-full'), 
+            { 
+                height: screen == 'home' ? 100 + statusBarHeight : 40 + statusBarHeight,
+                transform: [{ translateY: navbarTranslate }],
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                zIndex: 1000,
+                overflow: 'hidden'
+            },
+        ]}
+        onLayout={(event) => {
+            let {height} = event.nativeEvent.layout;
+            setClampedScroll(Animated.diffClamp(
+              Animated.add(
+                scrollAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 1],
+                  extrapolateLeft: 'clamp'
+                }),
+                offsetAnim
+              ), 0, height)
+            );
+        }}
+    >
       <Image
         style={[{ width: width, height: 40 + statusBarHeight, paddingTop: statusBarHeight }]}
-        source={require('../assets/HeaderBg.png')} />
+        source={require('../assets/HeaderBg.png')} 
+      />
+      
+      {screen == 'home' && <SecondHeader label={"GM! CoinEasy Frens!"} back={category ? () => resetCategory() : null} />}
     </Animated.View>
   )
 }
