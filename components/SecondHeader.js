efb19904ac6515f66e79c5e41497a0190925ea7a
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-04 13:07:20 +0200

    Fix shaky screen on scroll

--- components/SecondHeader.js
+++ components/SecondHeader.js
@@ -5,9 +5,10 @@ import { useTailwind } from 'tailwind-rn';
 import { GlobalContext } from "../contexts/GlobalContext";
 import { BackIcon, NotificationsIcon, SettingsIcon } from "./Icons";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import useStatusBarHeight from "../hooks/useStatusBarHeight";
 
 export default function SecondHeader({label, showBack = true, cta = "notifications", back}) {
-  const { setUser, orbis, postDetailsVis, setPostDetailsVis, setSettingsVis, setNotificationsVis } = useContext(GlobalContext);
+  const { setUser, orbis, postDetailsVis, setPostDetailsVis, setSettingsVis, setNotificationsVis, screen } = useContext(GlobalContext);
   const tailwind = useTailwind();
 
   function hidePostPane() {
@@ -22,30 +23,29 @@ export default function SecondHeader({label, showBack = true, cta = "notificatio
   }
 
   return(
-    <>
-    {/**<View style={{backgroundColor: "red", width: 20, height: 150, position: "absolute", top: 90, left:0}}></View>*/}
-    <View style={[tailwind('flex flex-row items-center px-3 pt-3 pb-0'), {minHeight: 50}]}>
+    <View style={{marginTop: screen == 'home' ? 0 : 40 + useStatusBarHeight(),}}>
+        <View style={[tailwind('flex flex-row items-center px-3 pt-3'), {minHeight: 50, backgroundColor: screen == 'home' ? '#f7f7f7' : 'white', paddingBottom: screen == 'home' ? 30 : 0 }]}>
 
-      {/** Back button */}
-      <View style={[tailwind('flex flex-1 items-start pt-1')]}>
-        <HeaderLabel label={label} showBack={showBack} back={back} />
-      </View>
+        {/** Back button */}
+        <View style={[tailwind('flex flex-1 items-start pt-1'),]}>
+            <HeaderLabel label={label} showBack={showBack} back={back} />
+        </View>
 
-      {/** Notifications button */}
-      {cta == "notifications" &&
-        <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-2')]} activeOpacity={0.7} onPress={() => setNotificationsVis(true)}>
-          <NotificationsIcon />
-        </TouchableOpacity>
-      }
+        {/** Notifications button */}
+        {cta == "notifications" &&
+            <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-2')]} activeOpacity={0.7} onPress={() => setNotificationsVis(true)}>
+            <NotificationsIcon />
+            </TouchableOpacity>
+        }
 
-      {/** Settings button */}
-      {cta == "settings" &&
-        <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-2')]} activeOpacity={0.7} onPress={() => setSettingsVis(true)}>
-          <SettingsIcon />
-        </TouchableOpacity>
-      }
+        {/** Settings button */}
+        {cta == "settings" &&
+            <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-2')]} activeOpacity={0.7} onPress={() => setSettingsVis(true)}>
+            <SettingsIcon />
+            </TouchableOpacity>
+        }
+        </View>
     </View>
-    </>
   )
 }
 
