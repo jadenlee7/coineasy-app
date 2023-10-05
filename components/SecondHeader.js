a386be82ecdb0ea94e0a9c5751181dced4b424f0
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-05 15:14:45 +0200

    switch to nav

--- components/SecondHeader.js
+++ components/SecondHeader.js
@@ -1,26 +1,16 @@
-import React, { useState, useContext, useEffect, useCallback } from "react";
-import { Keyboard, StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, KeyboardAvoidingView, TouchableHighlight } from 'react-native';
+import React, { useContext } from "react";
+import { Text, View, TouchableOpacity } from 'react-native';
+
 import * as Haptics from 'expo-haptics';
 import { useTailwind } from 'tailwind-rn';
+
 import { GlobalContext } from "../contexts/GlobalContext";
-import { BackIcon, NotificationsIcon, SettingsIcon } from "./Icons";
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import useStatusBarHeight from "../hooks/useStatusBarHeight";
+import { BackIcon, NotificationsIcon, SettingsIcon } from "./Icons";
 
 export default function SecondHeader({label, showBack = true, cta = "notifications", back}) {
-  const { setUser, orbis, postDetailsVis, setPostDetailsVis, setSettingsVis, setNotificationsVis, screen, profileSelected } = useContext(GlobalContext);
+  const { setSettingsVis, setNotificationsVis, screen, profileSelected } = useContext(GlobalContext);
   const tailwind = useTailwind();
-
-  function hidePostPane() {
-    Haptics.selectionAsync();
-    setPostDetailsVis(null);
-  }
-
-  async function logout() {
-    let res = await orbis.logout();
-    await AsyncStorage.removeItem("user-connected");
-    setUser(null);
-  }
 
   return(
     <View style={{marginTop: screen == 'home' ? 0 : 40 + useStatusBarHeight(),}}>
@@ -73,24 +63,24 @@ const HeaderLabel = ({label, showBack, back}) => {
         </>
       </TouchableOpacity>
     )
-  } else if(postDetailsVis && showBack) {
-    return(
-      <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-3')]} activeOpacity={0.7} onPress={() => hidePostPane()}>
-        <>
-          <BackIcon />
-          <Text style={[tailwind('text-slate-900 ml-3'), { fontFamily: "GmarketMedium" }]}>Back</Text>
-        </>
-      </TouchableOpacity>
-    )
-  } else if(profileSelected && showBack) {
-    return(
-      <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-3')]} activeOpacity={0.7} onPress={() => hideProfilePane()}>
-        <>
-          <BackIcon />
-          <Text style={[tailwind('text-slate-900 ml-3'), { fontFamily: "GmarketMedium" }]}>Back</Text>
-        </>
-      </TouchableOpacity>
-    )
+//   } else if(postDetailsVis && showBack) {
+//     return(
+//       <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-3')]} activeOpacity={0.7} onPress={() => hidePostPane()}>
+//         <>
+//           <BackIcon />
+//           <Text style={[tailwind('text-slate-900 ml-3'), { fontFamily: "GmarketMedium" }]}>Back</Text>
+//         </>
+//       </TouchableOpacity>
+//     )
+//   } else if(profileSelected && showBack) {
+//     return(
+//       <TouchableOpacity style={[tailwind('flex flex-row items-center rounded-md py-2 px-3')]} activeOpacity={0.7} onPress={() => hideProfilePane()}>
+//         <>
+//           <BackIcon />
+//           <Text style={[tailwind('text-slate-900 ml-3'), { fontFamily: "GmarketMedium" }]}>Back</Text>
+//         </>
+//       </TouchableOpacity>
+//     )
   } else {
     return(
       <Text style={[tailwind('text-slate-900 px-2'), { fontSize: 16, fontFamily: "GmarketBold", lineHeight: 20 }]}>{label}</Text>
