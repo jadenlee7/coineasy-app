8a1e513a111407911fb87b2707a561775d885fc1
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-05 15:11:07 +0200

    switch to nav

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,53 +1,110 @@
-import React, { useState, useContext, useEffect, useCallback } from "react";
-import { GlobalContext } from "../contexts/GlobalContext";
+import React, { useState, useContext, useEffect } from "react";
+import { View, TouchableOpacity, Image, ActivityIndicator, Dimensions, Text, Keyboard } from 'react-native';
+
 import { useTailwind } from 'tailwind-rn';
-import { ScrollView, StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ActivityIndicator, RefreshControl, TouchableHighlight } from 'react-native';
-import Header from "../components/Header";
-import Post from "../components/Post";
-import Button from "../components/Button";
-import { UserPfp, Username } from "../components/User";
-import SecondHeader from "../components/SecondHeader";
+
+import { GlobalContext } from "../contexts/GlobalContext";
 import ProfileDetails from "../components/ProfileDetails";
+import { SettingsIcon } from "../components/Icons";
+import useStatusBarHeight from "../hooks/useStatusBarHeight";
+import * as Haptics from 'expo-haptics';
 
-export default function Profile() {
-  const { user, setUser, orbis } = useContext(GlobalContext);
-  const tailwind = useTailwind();
-  const [profile, setProfile] = useState();
+import Modal from "../components/Modal";
+import Button from "../components/Button";
 
-  useEffect(() => {
-    getProfile();
-  }, [user]);
-
-  async function getProfile() {
-    const { data, error } = await orbis.getProfile(user.did);
-    setProfile(data);
-  }
-
-  async function logout() {
-    let res = await orbis.logout();
-    setUser(null);
-  }
-  return(
-    <>
-      <SecondHeader label="" showBack={false} cta="settings" />
-
-      {profile ?
-        <ProfileDetails profile={user} />
-      :
-        <View style={tailwind('flex flex-row items-start w-full justify-center mt-6 flex-1')}>
-          <ActivityIndicator style={{marginTop: 25}} size="small" color="#020617" />
-        </View>
-      }
+const Profile = ({ navigation, route }) => {
+    const { user, setUser, orbis } = useContext(GlobalContext);
+    const tailwind = useTailwind();
+    const statusBarHeight = useStatusBarHeight();
 
 
+    const [profile, setProfile] = useState();
+    const [showModal, setShowModal] = useState(false)
 
-      {/**
-      <View style={tailwind('flex flex-col')}>
-        <TouchableHighlight style={[tailwind(`bg-slate-50 px-8 py-6`)]} onPress={() => logout()}>
-          <Text style={[tailwind(`text-slate-900`), { fontSize: 12, fontFamily: "GmarketBold", lineHeight: 15 }]}>Logout</Text>
-        </TouchableHighlight>
-      </View>
-      */}
-    </>
+    useEffect(() => {
+        getProfile();
+        navigation.addListener('tabPress', (e) => {
+            Haptics.selectionAsync();
+        });
+    }, [user]);
+
+    async function getProfile() {
+        const { data, error } = await orbis.getProfile(user.did);
+        setProfile(data);
+    }
+
+    function hideSettings() {
+        setShowModal(false);
+        Keyboard.dismiss()
+        Haptics.selectionAsync();
+    }
+
+    async function logout() {
+        Haptics.selectionAsync();
+        setSettingsVis(false);
+        AsyncStorage.removeItem("user-connected");
+        let res = await orbis.logout();
+        console.log("res:", res);
+
+        let providerType = await AsyncStorage.getItem("provider-type");
+        if(providerType == "wallet-connect") {
+            provider?.disconnect();
+        }
+
+        setUser(null);
+    }
+
+    async function openHelp() {
+        Haptics.selectionAsync();
+        let result = await WebBrowser.openBrowserAsync("https://drive.google.com/file/d/1x8ZvprutJSuv96KVz3vLyXHWXwi8AaVS/view?usp=sharing");
+    }
+
+    function openPrivacyPolicy() {
+        Haptics.selectionAsync();
+        alert("Coming soon");
+    }
+
+    function openTerms() {
+        Haptics.selectionAsync();
+        alert("Coming soon");
+    }
+
+    return(
+        <>
+            <Image
+                style={{ 
+                    width: Dimensions.get('window').width,
+                    height: 40 + statusBarHeight,
+                    paddingTop: statusBarHeight,
+                }}
+                source={require('../assets/HeaderBg.png')} 
+            />
+
+            <TouchableOpacity style={{position: 'absolute',top: 90,right: 30,zIndex: 2,}}  activeOpacity={0.7} onPress={() => setShowModal(true)}>
+                <SettingsIcon />
+            </TouchableOpacity>
+
+            {profile ?
+                <ProfileDetails profile={user} />
+            :
+                <View style={tailwind('flex flex-row items-start w-full justify-center mt-6 flex-1')}>
+                    <ActivityIndicator style={{marginTop: 25}} size="small" color="#020617" />
+                </View>
+            }
+
+            {showModal && (
+                <Modal visible hide={() => hideSettings()} animateModal={true} bottomDuration={200} bottomStart={-100}>
+                    <View style={[tailwind('flex flex-col w-full p-5')]}>
+                        <Text style={[tailwind('text-primary mb-5')]}>Settings & Privacy</Text>
+                        <Button color="rounded-gray" title="Help" style={{marginBottom: 10}} onPress={() => openHelp()} />
+                        <Button color="rounded-gray" title="Privacy Policy" style={{marginBottom: 10}} onPress={() => openPrivacyPolicy()} />
+                        <Button color="rounded-gray" title="Terms and Conditions" style={{marginBottom: 10}} onPress={() => openTerms()} />
+                        <Button color="rounded-red" title="Logout" onPress={() => logout()} style={{marginBottom: 30}}  />
+                    </View>
+                </Modal>
+            )}
+        </>
   )
 }
+
+export default Profile
