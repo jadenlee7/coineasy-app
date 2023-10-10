39de8003a9722c6e58c46cea20dfc0018357448a
Yeloda <86018496+Yeloda@users.noreply.github.com>
2023-10-10 16:05:22 +0200

    multiple updates

--- screens/Navigation/ProfileSelected.js
+++ screens/Navigation/ProfileSelected.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
+import { ActivityIndicator, Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
 
 import * as Haptics from 'expo-haptics';
 import { useTailwind } from "tailwind-rn";
@@ -27,36 +27,24 @@ const ProfileSelected = ({navigation, route}) => {
         setProfile(data);
     }
 
-
-
     return(
         <View style={{flex: 1}}>
-            <Image
-                style={{ 
-                    width: Dimensions.get('window').width,
-                    height: 40 + statusBarHeight,
-                    paddingTop: statusBarHeight,
-                }}
-                source={require('../../assets/HeaderBg.png')} 
-            />
-
-            <View style={{backgroundColor: 'white',flexDirection: 'row',justifyContent: 'space-between',alignItems: 'center',paddingLeft: 10,paddingRight: 20,paddingTop: 5,}}>
-                <TouchableOpacity onPress={() => {Haptics.selectionAsync();navigation.goBack()}}>
-                    <View style={{zIndex:100000, justifyContent: 'center',alignItems: 'center',margin: 15, backgroundColor: 'white',flexDirection:'row',}}>
-                        <BackIcon />
-                        <Text style={[tailwind('text-slate-900 ml-3'), { fontFamily: "GmarketMedium" }]}>Back</Text>
-                    </View>
-                </TouchableOpacity>
-
-                <TouchableOpacity activeOpacity={0.7} onPress={() => {Haptics.selectionAsync();navigation.navigate('Notifications')}}>
-                    <NotificationsIcon />
-                </TouchableOpacity>
-            </View>
 
             {profile ? (
-                <ProfileDetails profile={profile?.details} pfpMarginTop={0}/>
+                <ProfileDetails profile={profile?.details} pfpMarginTop={0} type='selected'/>
             ) : (
-                <View style={{backgroundColor: 'white',height: 800, width:'100%'}} />
+                <View style={tailwind('w-full flex-1 bg-white')}>
+                    <Image
+                        style={{ 
+                            width: Dimensions.get('window').width,
+                            height: 40 + statusBarHeight,
+                            paddingTop: statusBarHeight,
+                        }}
+                        source={require('../../assets/HeaderBg.png')} 
+                    />
+                    
+                    <ActivityIndicator style={{marginTop: 25}} size="small" color="#020617" />
+                </View>
             )}
         </View>
     )
